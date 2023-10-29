548a319410780e773adaf2fea98ea4c656953638
Tóla Bowen Maccurtain <tolabowenmaccurtain@Tolas-MacBook-Air.local>
2023-10-29 17:59:12 +0000

    quiz game foundations

--- Questions.java
+++ Questions.java
@@ -1,6 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+/*
 public class Questions {
-    public static void main(String[] args){
-        System.out.println("Welcome to the Question Game");
+    / public static void main(String[] args) {
+        String url = "jdbc:sqlite:/path/to/your/database.db"; // Replace with your database path
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+             Perform SQL operations here
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
- 
+*/
